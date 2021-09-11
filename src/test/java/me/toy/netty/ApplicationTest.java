8f0e9812072d7290a95f2af3e5662c7e41392c77
slamdunk7575 <slamdunk7575@gmail.com>
2021-09-11 17:35:23 +0900

    (feat) init commit

---
+++ src/test/java/me/toy/netty/ApplicationTest.java
@@ -0,0 +1,12 @@
+package me.toy.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
