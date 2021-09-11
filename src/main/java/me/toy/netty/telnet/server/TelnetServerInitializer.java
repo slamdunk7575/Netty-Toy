d04668739aa071bd95700e1a53c6ed186ff1ebf4
slamdunk7575 <slamdunk7575@gmail.com>
2021-09-11 17:42:16 +0900

    (feat) 채널 Pipeline 등록

---
+++ src/main/java/me/toy/netty/telnet/server/TelnetServerInitializer.java
@@ -0,0 +1,28 @@
+package me.toy.netty.telnet.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import me.toy.netty.telnet.handler.TelnetServerHandler;
+
+public class TelnetServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    private static final StringDecoder DECODER = new StringDecoder();
+    private static final StringEncoder ENCODER = new StringEncoder();
+
+    private static final TelnetServerHandler SERVER_HANDLER = new TelnetServerHandler();
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+        pipeline.addLast(DECODER);
+        pipeline.addLast(ENCODER);
+        pipeline.addLast(SERVER_HANDLER);
+    }
+}
